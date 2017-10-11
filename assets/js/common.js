1fa302c0ec0c7c703ea6f10054d0d9d784aaab26
kimyh5392 <mk.kimyh@gmail.com>
2017-10-11 18:32:41 +0900

    inc > header > 검색폼 수정

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,10 +9,10 @@
     
     
     // 검색    
-//    $(document).on('click', '.global-search .search-link', function(e) {
-//        $('.global-search').addClass('on');
-//        e.preventDefault();
-//    });
+    $(document).on('click', '.global-search .search-link', function(e) {
+        $('.global-search').addClass('on');
+        e.preventDefault();
+    });
 //    $(document).on('click', '.global-search .btn-search', function() {
 //        $('.global-search').removeClass('on');
 //    });
