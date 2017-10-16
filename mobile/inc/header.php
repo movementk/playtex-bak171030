e701b61fb1278064d4f9bce9d98cfb299aed2254
mk-kimjh <mk.kimjh@gmail.com>
2017-10-16 17:10:19 +0900

    모바일 레이아웃 추가

---
+++ mobile/inc/header.php
@@ -0,0 +1,25 @@
+<header id="header">
+	<div class="util">
+		<h1 class="logo"><a href="/mobile/"><img src="/design/playtex/mobile/assets/images/logo.png" alt="플레이텍스"></a></h1>
+		<ul>
+			<li><a class="cart" href="#"><span class="sr-only">장바구니</span></a></li>
+			<li><a class="search" href="#"><span class="sr-only">검색</span></a></li>
+			<li>
+				<a class="menu" href="#nav">
+					<span class="sr-only">메뉴보기</span>
+					<span class="bar"></span>
+					<span class="bar short"></span>
+					<span class="bar"></span>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div class="category">
+		<ul>
+			<li><a href="#">ITEM</a></li>
+			<li><a href="#">EVENT</a></li>
+			<li><a href="#">COUPON</a></li>
+			<li><a href="#">SALE</a></li>
+		</ul>
+	</div>
+</header>
