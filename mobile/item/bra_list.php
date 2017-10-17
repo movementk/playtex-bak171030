f717566c8136e74eb149297bd5f762886ec87522
kimyh5392 <mk.kimyh@gmail.com>
2017-10-17 18:45:58 +0900

    design > list 작업중 . . .

--- mobile/item/bra_list.php
+++ mobile/item/bra_list.php
@@ -7,26 +7,242 @@
 		<div class="page-header">
 		    <h2 class="title">ITEM</h2>
 		</div>
-		<article>
-		    <div class="container">
-		        <div class="article-header">
-                    <ul class="product-list">
-                        <li class="active"><a href="#">BRA</a></li>
-                        <li><a href="#">PANTY</a></li>
-                        <li><a href="#">1SET</a></li>
-                        <li><a href="#">2SET</a></li>
-                        <li><a href="#">FULL SET</a></li>
-                        <li><a href="#">ETC</a></li>
-                    </ul>
-		        </div>
-		        <div class="article-content">
-		            
-		        </div>
-		    </div>
-		</article>
+        
+        <nav class="product-nav">
+            <div class="container">
+                <ul>
+                    <li class="active"><a href="/mobile/item/bra_list.php">BRA</a></li>
+                    <li><a href="/mobile/item/panty_list.php">PANTY</a></li>
+                    <li><a href="/mobile/item/1set_list.php">1SET</a></li>
+                    <li><a href="/mobile/item/2set_list.php">2SET</a></li>
+                    <li><a href="/mobile/item/fullset_list.php">FULL SET</a></li>
+                    <li><a href="/mobile/item/shaper_list.php">SHAPER</a></li>
+                    <li><a href="/mobile/item/etc_list.php">ETC</a></li>
+                </ul>
+            </div>
+        </nav>
+        
+        <article>
+            <div class="article-header">
+                <div class="container">
+                    <h3 class="article-title">BRA</h3>
+                    <p class="summary">
+                        18시간의 자유로움, 스트레스프리<br>
+                        노와이어 기술력의 집약체
+                    </p>
+                    <div class="best-product">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <figure>
+                                        <img src="/design/playtex/mobile/assets/images/item/slider_img01.jpg" class="img-responsive" alt="">
+                                        <figcaption>
+                                            <small>BEST</small>
+                                            <p class="number">01</p>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="details">
+                                        <p class="product-title">플레이텍스 에센셜 노와이어 4종세트 PXW7E1921(A)_T </p>
+                                        <ul class="recommend-list">
+                                            <li class="md">MD</li>
+                                            <li class="hot">HOT</li>
+                                            <li class="new">NEW</li>
+                                        </ul>
+                                        <p class="payment">39,900</p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <figure>
+                                        <img src="/design/playtex/mobile/assets/images/item/slider_img01.jpg" class="img-responsive" alt="">
+                                        <figcaption>
+                                            <small>BEST</small>
+                                            <p class="number">02</p>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="details">
+                                        <p class="product-title">플레이텍스 에센셜 노와이어 4종세트 PXW7E1921(A)_T </p>
+                                        <ul class="recommend-list">
+                                            <li class="md">MD</li>
+                                            <li class="hot">HOT</li>
+                                            <li class="new">NEW</li>
+                                        </ul>
+                                        <p class="payment">39,900</p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <figure>
+                                        <img src="/design/playtex/mobile/assets/images/item/slider_img01.jpg" class="img-responsive" alt="">
+                                        <figcaption>
+                                            <small>BEST</small>
+                                            <p class="number">03</p>
+                                        </figcaption>
+                                    </figure>
+                                    <div class="details">
+                                        <p class="product-title">플레이텍스 에센셜 노와이어 4종세트 PXW7E1921(A)_T </p>
+                                        <ul class="recommend-list">
+                                            <li class="md">MD</li>
+                                            <li class="hot">HOT</li>
+                                            <li class="new">NEW</li>
+                                        </ul>
+                                        <p class="payment">39,900</p>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="article-content">
+                <div class="container">
+                    <div class="select-menu">
+                        <div class="dropdown">
+                            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                인기순으로 보기
+                                <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                                <li><a href="#">인기순으로 보기</a></li>
+                                <li><a href="#">낮은금액으로 보기</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="product-list">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <p class="figure">
+                                        <img src="/design/playtex/mobile/assets/images/item/product_list_img01.jpg" class="img-responsive" alt="">
+                                    </p>
+                                    <div class="details">
+                                        <p class="product-title">플레이텍스 에센셜 노와이어 브라팬티 4종 PXW7E2527_T </p>
+                                        <ul class="recommend-list">
+                                            <li class="hot">HOT</li>
+                                        </ul>
+                                        <p class="payment">39,900</p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <p class="figure">
+                                        <img src="/design/playtex/mobile/assets/images/item/product_list_img02.jpg" class="img-responsive" alt="">
+                                    </p>
+                                    <div class="details">
+                                        <p class="product-title">플레이텍스 에센셜 노와이어 브라팬티 4종 PXW7E2527_T </p>
+                                        <ul class="recommend-list">
+                                            <li class="hot">HOT</li>
+                                        </ul>
+                                        <p class="payment">39,900</p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <p class="figure">
+                                        <img src="/design/playtex/mobile/assets/images/item/product_list_img01.jpg" class="img-responsive" alt="">
+                                    </p>
+                                    <div class="details">
+                                        <p class="product-title">플레이텍스 에센셜 노와이어 브라팬티 4종 PXW7E2527_T </p>
+                                        <ul class="recommend-list">
+                                            <li class="hot">HOT</li>
+                                        </ul>
+                                        <p class="payment">39,900</p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <p class="figure">
+                                        <img src="/design/playtex/mobile/assets/images/item/product_list_img02.jpg" class="img-responsive" alt="">
+                                    </p>
+                                    <div class="details">
+                                        <p class="product-title">플레이텍스 에센셜 노와이어 브라팬티 4종 PXW7E2527_T </p>
+                                        <ul class="recommend-list">
+                                            <li class="hot">HOT</li>
+                                        </ul>
+                                        <p class="payment">39,900</p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <p class="figure">
+                                        <img src="/design/playtex/mobile/assets/images/item/product_list_img01.jpg" class="img-responsive" alt="">
+                                    </p>
+                                    <div class="details">
+                                        <p class="product-title">플레이텍스 에센셜 노와이어 브라팬티 4종 PXW7E2527_T </p>
+                                        <ul class="recommend-list">
+                                            <li class="hot">HOT</li>
+                                        </ul>
+                                        <p class="payment">39,900</p>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <p class="figure">
+                                        <img src="/design/playtex/mobile/assets/images/item/product_list_img02.jpg" class="img-responsive" alt="">
+                                    </p>
+                                    <div class="details">
+                                        <p class="product-title">플레이텍스 에센셜 노와이어 브라팬티 4종 PXW7E2527_T </p>
+                                        <ul class="recommend-list">
+                                            <li class="hot">HOT</li>
+                                        </ul>
+                                        <p class="payment">39,900</p>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <nav class="paging" aria-label="Page navigation">
+                        <ul class="pagination">
+                            <li class="first">
+                                <a href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <li class="prev">
+                                <a href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <li><a href="#">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">4</a></li>
+                            <li><a href="#">5</a></li>
+                            <li class="next">
+                                <a href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                            <li class="last">
+                                <a href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    
+                </div>
+            </div>
+        </article>
 
 	</main>
 	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/mobile/inc/footer.php"); ?>
 	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/mobile/inc/docfoot.php"); ?>
+	<script src="/design/playtex/mobile/assets/jquery.bxslider/jquery.bxslider.min.js"></script>
+    <script>
+        (function($) {
+            $('.best-product > ul').bxSlider({
+                //auto: true,
+                controls: false
+            });
+        })(jQuery);
+    </script>
 </body>
 </html>
