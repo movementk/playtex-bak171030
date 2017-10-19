070f322f5f73f91e4396853c5cf6ceaff5b80b57
kimyh5392 <mk.kimyh@gmail.com>
2017-10-19 15:58:06 +0900

    마이페이지 작업중 #2

--- mobile/mypage/wish_list.php
+++ mobile/mypage/wish_list.php
@@ -24,7 +24,49 @@
         </nav>
         <div class="container">
 		    <article class="coupon-info">
-		        
+		        <div class="selection-all">
+                    <p class="checking">
+                        <input type="checkbox">
+                    </p>
+		            <p class="selection-btn">
+		                <a href="#" class="btn btn-xs" role="button">전체삭제</a>
+		            </p>
+		        </div>
+		        <ul class="list">
+		            <li>
+                        <p class="checking">
+                            <input type="checkbox">
+                        </p>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <figure>
+                                    <img src="/design/playtex/mobile/images/mypage/details_info_img.gif" class="img-responsive" alt="">
+                                </figure>
+                            </div>
+                            <div class="col-xs-8">
+                                <h4 class="p-title">★57%+세컨3종증정★ [Playtex]플레이텍스 에센셜 노와이어 브라팬티 11종세트 PXI03_08 </h4>
+                            </div>
+                        </div>
+                        <div class="product-info">
+                            <dl>
+                                <dt>수량</dt>
+                                <dd>1</dd>
+                                <dt>재고</dt>
+                                <dd>있음</dd>
+                                <dt>적립금</dt>
+                                <dd>630</dd>
+                                <dt>가격</dt>
+                                <dd>71,900</dd>
+                            </dl>
+                            <div class="btn-area">
+                                <p>
+                                    <a href="#" class="btn btn-gray" role="button">장바구니 담기</a>
+                                    <a href="#" class="btn btn-beige" role="button">삭제하기</a>
+                                </p>
+                            </div>
+                        </div>
+		            </li>
+		        </ul>
 		    </article>
         </div>
 	</main>
